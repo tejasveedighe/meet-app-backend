4ba8c18e0d3665f2d5ffe0e44c2068067730a526
tejasveedighe <tejasveedighe@gmail.com>
2023-09-26 12:01:18 +0530

    using post to send the auth token

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const express = require("express")
-const app = express()
-const port = 3001
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+	res.send("Hello World");
+});
 
-app.listen(port, () => console.log(`Server Running on Port ${port}`))
+app.post("/get-token", (req, res) => {
+	console.log(req.body);
+	res.json({ authToken: "avcd", body: res.body });
+});
+
+app.listen(port, () => console.log(`Server Running on Port ${port}`));
