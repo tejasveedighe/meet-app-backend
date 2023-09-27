00b3121bbf0fef17baa78fe9c54b4abfb922ed93
tejasveedighe <tejasveedighe@gmail.com>
2023-09-27 10:25:31 +0530

    Update the get-token method

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
@@ -12,9 +13,25 @@ app.get("/", (req, res) => {
 	res.send("Hello World");
 });
 
+const jwt = require("jsonwebtoken");
+
 app.post("/get-token", (req, res) => {
-	console.log(req.body);
-	res.json({ authToken: "avcd", body: res.body });
+	const API_KEY = process.env.API_KEY;
+	const SECRET = process.env.SECRET;
+
+	const options = {
+		expiresIn: "120m",
+		algorithm: "HS256",
+	};
+	const payload = {
+		apikey: API_KEY,
+		permissions: [`allow_join`], // `ask_join` || `allow_mod`
+		version: 2,
+		roles: ["CRAWLER"],
+	};
+
+	const token = jwt.sign(payload, SECRET, options);
+	res.json({ authToken: token });
 });
 
 app.listen(port, () => console.log(`Server Running on Port ${port}`));
